ef69fcfee7ab17fb72e4c1ae404b538fdee34fb0
MESoliman <seliman1611@gmail.com>
2020-01-02 19:56:02 -0800

    Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,8 +22,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
-import { MatDialogModule, MatFormFieldModule, MatInputModule, MatCheckboxModule } from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatFormFieldModule,
+  MatInputModule, MatCheckboxModule, MatSelectModule,
+  MatSlideToggleModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -51,7 +53,10 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
